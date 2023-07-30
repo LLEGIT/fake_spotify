df454f8d40b836c5d5ddeaaa37de3ccebe7e8b12
LLEGIT <theogillet.developpement@gmail.com>
2023-07-30 13:15:02 +0200

    refactor: added dockerized db

--- spotify-api/app/models/connection/index.js
+++ spotify-api/app/models/connection/index.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "tweet_academy",
-    password: "admin",
-    database: "database_music",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
+
+console.log(connection)
 
 module.exports = connection;
